ab1bb7c272eb37a3228b648b7a8fce51bc3bbd72
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-20 22:23:56 +0600

    added cart page

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,144 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { addItem, cartItem, removeItem } from "@/store/cartSlice";
+import { RooteState } from "@/store/store";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+
+const Cart = () => {
+    const dispatch = useDispatch();
+  // Get our cart items
+  const items = useSelector((state: RooteState) => state.cart.item);
+  // Calculating Total Quantity.
+  const totalQuantity = items.reduce((total, item) => total + item.quantity, 0);
+  // Calculate the total price
+  const totalPrice = items
+    .reduce((total, item) => total + item.price * item.quantity, 0)
+    .toFixed(2);
+
+  // Calculate vat (15%)
+  const vat = (+totalPrice * 0.15).toFixed(2);
+
+  // Total price with vat
+  const totalPriceWithVat = (+totalPrice + +vat).toFixed(2);
+
+  // Get authenticate user
+  const {user} = useUser();
+
+  // Add item
+  const addItemHandler = (item:cartItem)=>{
+    dispatch(addItem(item));
+  }
+
+    // remove item
+    const removeItemHandler = (id:number)=>{
+        dispatch(removeItem({id}));
+      }
+
+  return (
+    <div className="mt-8 min-h-[60vh]">
+      {/* Check if the cart is empty */}
+      {items.length == 0 && (
+        <div className="flex items-center justify-center flex-col w-full h-[80vh]">
+          <Image
+            src="/images/cart.svg"
+            alt="empty_cart"
+            width={400}
+            height={400}
+            className="object-cover mx-auto"
+          />
+          <h1 className="mt-8 font-semibold text-2xl">Your cart is empty</h1>
+          <Link href="/">
+            <Button className="mt-4">Shop Now</Button>
+          </Link>
+        </div>
+      )}
+      {/* if cart item exist */}
+      {items.length > 0 && (
+        <div className="md:w-4/5 w-[95%] mx-auto grid grid-cols-1 xl:grid-cols-6 gap-12">
+          {/* cart items */}
+          <div className="rounded-lg shadow-md overflow-hidden xl:col-span-4">
+            <h1 className="p-4 text-xl sm:text-2xl md:text-3xl font-bold text-white bg-blue-700">
+              Your Cart is({totalQuantity}items)
+            </h1>
+            {items.map((item) => {
+              return (
+                <div key={item.id}>
+                  <div className="flex pb-6 mt-2 p-5 border-b-[1.5px] border-opacity-25 border-gray-700 items-center space-x-10">
+                    <div>
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={180}
+                        height={180}
+                      />
+                    </div>
+                    <div>
+                      <h1 className="md:text-xl text-base font-bold text-black">
+                        {item.title}
+                      </h1>
+                      <h1 className="md:text-lg text-sm font-semibold">
+                        Category: {item.category}
+                      </h1>
+                      <h1 className="md:text-2xl text-lg font-bold text-blue-950">
+                        ${item.price}
+                      </h1>
+                      <h1 className="md:text-lg text-sm font-semibold">
+                        Quantity: {item.quantity}
+                      </h1>
+                      <div className="flex items-center mt-4 space-x-2">
+                        <Button onClick={()=>{addItemHandler(item)}}>Add More</Button>
+                        <Button onClick={()=>{removeItemHandler(item.id)}} variant={"destructive"}>Remove</Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {/* Cart summary */}
+          <div className="xl:col-span-2">
+            <div className="bg-indigo-950 sticky top-[25vh] p-6 rounded-lg">
+              <h1 className="text-center mt-8 mb-8 text-white text-2xl font-semibold">
+                Summary
+              </h1>
+              <div className="w-full h-[1.2px] bg-white bg-opacity-20"></div>
+              <div className="flex mt-4 text-xl uppercase font-semibold text-white items-center justify-between">
+                <span>Subtotal</span>
+                <span>${totalPrice}</span>
+              </div>
+              <div className="flex mt-10 mb-10 text-xl uppercase font-semibold text-white items-center justify-between">
+                <span>Vat</span>
+                <span>${vat}</span>
+              </div>
+              <div className="flex mb-6 text-xl uppercase font-semibold text-white items-center justify-between">
+                <span>Shipping</span>
+                <span>Free</span>
+              </div>
+              <div className="w-full h-[1.2px] bg-white bg-opacity-20"></div>
+              <div className="flex mt-6 mb-6 text-xl uppercase font-semibold text-white items-center justify-between">
+                <span>Total</span>
+                <span>${totalPriceWithVat}</span>
+              </div>
+              {!user && (
+                <Link href="/sign-in">
+                    <Button className="bg-orange-500 w-full">Sign In to Checkout</Button>
+                </Link>
+              )}
+              {
+                user && (
+                    // paypal button
+                    <Button className="w-full bg-orange-500">Paypal</Button>
+                )
+              }
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
